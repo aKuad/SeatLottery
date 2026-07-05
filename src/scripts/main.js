755391a71b875190abc4321e6fe7678aae253c23
aKuad <53811809+aKuad@users.noreply.github.com>
2026-07-05 16:16:18 +0900

    Members input check missing on delete button
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -131,6 +131,7 @@ window.addEventListener("load", () => {
   // Button - Members input delete
   document.querySelector("#members-delete").addEventListener("click", () => {
     document.querySelector("#input-members").value = "";
+    document.querySelector("#input-members").dispatchEvent(new Event("input"));  // For run input check
   });
 
   // Button - Layout file input
